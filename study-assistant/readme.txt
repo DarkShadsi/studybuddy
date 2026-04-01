6950f339cb4b5d86c9ead3480318197c1b9bb613
DarkShadsi <depaudhonvincent24@gmail.com>
2026-04-01 08:22:26 +0800

    Created function to test DAOs Main can now be run
    - Created readme.txt for running the source code
    - Updated pom.xml
    - Added some temporary methods for testing implemented methods

---
+++ study-assistant/readme.txt
@@ -0,0 +1,3 @@
+HOW TO RUN:
+1. cd to "C:\Users\Admin\Documents\Repos\StudyAssistantApp\studybuddy\study-assistant"
+2. run: mvn compile exec:java
--- study-assistant/src/main/java/com/studyapp/Main.java
+++ study-assistant/src/main/java/com/studyapp/Main.java
@@ -1,5 +1,12 @@
 package com.studyapp;
 
+import java.util.Scanner;
+
+import com.studyapp.controller.CustomException;
+import com.studyapp.controller.MainController;
+import com.studyapp.db.DatabaseConnection;
+import com.studyapp.model.Deck;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: Initialize DatabaseConnection
@@ -7,5 +14,75 @@ public class Main {
         // TODO: Instantiate Services
         // TODO: Instantiate Controllers
         // TODO: Create and show MainFrame
+
+        new Main().test();
+
+    }
+
+        //------ TEMPORARY FOR TESTING DAOs ONLY -----------
+    void test(){
+        Scanner scanner = new Scanner(System.in);
+        MainController mc = new MainController();
+
+        System.out.print("Enter db username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter db password: ");
+        String password = scanner.nextLine();
+
+        try{
+            mc.login(username, password);
+
+            while (true) {
+                System.out.println("\n1. Add deck\n2. Update deck\n3. Delete deck\n4. Find deck");
+                System.out.println("5. Add card\n6. Update card\n7. Delete card\n8. Delete card");
+                System.out.print("Enter number: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        System.out.print("Deck name: ");
+                        String deckName = scanner.nextLine();
+                        System.out.print("Decription: ");
+                        String description = scanner.nextLine();
+                        mc.createDeck(deckName, description);
+                        break;
+                    case 2:
+                        System.out.print("Deck ID to update: ");
+                        int deckID = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Deck name: ");
+                        String newDeckName = scanner.nextLine();
+                        System.out.print("Decription: ");
+                        String newDescription = scanner.nextLine();
+                        mc.updateDeck(deckID, newDeckName, newDescription);
+                        break;
+                    case 3:
+                        System.out.print("Deck ID to be deleted: ");
+                        int deckIDToDelete = scanner.nextInt();
+                        scanner.nextLine();
+                        mc.deleteDeck(deckIDToDelete);
+                        break;
+                    case 4:
+                        System.out.print("Deck ID to find: ");
+                        int deckIDToFind = scanner.nextInt();
+                        scanner.nextLine();
+                        Deck foundDeck = mc.findDeck(deckIDToFind);
+                        if(foundDeck != null){
+                            System.out.println("Deck ID: " + foundDeck.getDeckID());
+                            System.out.println("Name: " + foundDeck.getName());
+                            System.out.println("Descrpition: " + foundDeck.getDescription());
+                            System.out.println("Created at: " + foundDeck.getCreatedAt());
+                        }else{
+                            System.out.println("Deck not found!");
+                        }
+                        break;
+
+
+                }
+            }
+        }catch(CustomException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
